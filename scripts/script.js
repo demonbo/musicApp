100535ff728796cbf2f9ae3caee01f4cfc9e5de2
Gia Nguyen Hoang <nhgia@tma.com.vn>
2017-10-12 18:08:13 +0700

    co Oct 12

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,22 @@
-$(document).ready(function(){
-    var bl = true;
-    $("#btnMenu").click(function(){
-//                $(this).hide();
-        console.log('hello');
-        if(bl) {
-            $("#menu").css("display", "inline-block");
-            // $("section").css("width", "50%");
-        } else {
-            $("#menu").css("display", "none");
-            // $("section").css("width", "100%");
-        }
-        bl = !bl;
-
-    });
-
+// $(document).ready(function(){
+//     var bl = true;
+//     $("#btnMenu").click(function(){
+// //                $(this).hide();
+//         console.log('hello');
+//         if(bl) {
+//             $("#menu").css("display", "inline-block");
+//             // $("section").css("width", "50%");
+//         } else {
+//             $("#menu").css("display", "none");
+//             // $("section").css("width", "100%");
+//         }
+//         bl = !bl;
+//
+//     });
+//
+// });
+$("#menu-toggle").click(function(e) {
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled");
 });
+
